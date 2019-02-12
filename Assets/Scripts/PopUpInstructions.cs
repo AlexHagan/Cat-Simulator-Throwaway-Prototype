668c7297af711082e0d76d9ed1d48afadadf331d
Kevin Allen <kma1660@gmail.com>
2019-02-11 19:12:59 -0500

    Cindys menu

---
+++ Assets/Scripts/PopUpInstructions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpInstructions : MonoBehaviour
+{
+    public GameObject instructionsButtonCanvas;
+
+    // Update is called once per frame
+    void Start()
+    {
+        Instantiate(instructionsButtonCanvas,instructionsButtonCanvas.transform.position, instructionsButtonCanvas.transform.rotation);
+
+    }
+
+    void OnClick()
+    {
+     
+    }
+}
