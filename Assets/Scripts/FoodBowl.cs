dbb500c21f72eb5a8f83e57b8c7bf51de7f1634d
AlexHagan <alex.h8008@gmail.com>
2019-02-14 19:08:31 -0500

    Feeding mechanism implemented. Click on food bowl to refill it.

---
+++ Assets/Scripts/FoodBowl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBowl : MonoBehaviour
+{
+	GameObject kibble;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        kibble = GameObject.Find("Cat Food");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnMouseDown()
+	{
+		kibble.SetActive(true);
+	}
+}
